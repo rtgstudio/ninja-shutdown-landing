1c4c7cc0b94dbe2c5069ac749207426da5bf168b
Pankaj Goyal <pkjg1807@gmail.com>
2019-08-02 16:02:17 +0530

    edit about page

--- src/App.js
+++ src/App.js
@@ -14,46 +14,37 @@ class App extends Component{
       <div className="App">
         <header id="header">
           <div id="logo-name">
-            <img id="header-img" src={logo} alt="company-logo" height="50px" />
+            <img id="header-img" src={logo} alt="company-logo" height="60px" />
               <p id="company-name">R.T.G. Studios</p>
           </div>
           <nav id="nav-bar">
             <ul class="ul-container">
               <li><a class="nav-link" href="#services">Services</a></li>
-              <li><a class="nav-link" href="#features">Features</a></li>
+              <li className="nav-link">Features</li>
+              <li className="nav-link">Contact</li>
             </ul>
           </nav>
         </header>
-        <form id="form" action="https://www.freecodecamp.com/email-submit">
-          <h1 id="form-h1"><p>A simple to use application which allows<br /> you to easily schedule the computer shutdown
-but also trigger the system reboot or
-log off on demand</p></h1>
+        <form id="form">
+          <h1 id="form-h1"><p>We'll be here with our product Ninja Shutdown in some days.<br />Enter Your Email to get more info.  </p></h1>
           <input type="email" name="email" id="email" placeholder="Enter your Email" required />
-          <input type="submit" id="submit" value="Get Started" />
+          <input type="submit" id="submit" value="Submit" />
         </form>
         <div class="container">
-          <section id="services">
-            <div className="grid-header">
-              <h1>Our <span>services</span></h1>
-              <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy<br />nibh euismod tincidunt ut laoreet dolore magna.</p>
-            </div>
-            <div class="hidden-grid">
-              <div class="item-box">
-                <div class="icon">
-                  <img class="img-icon" alt="content-writing" src="https://image.flaticon.com/icons/svg/660/660756.svg" />
-                </div>
-                <h2>Content writing</h2>
-                <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut.</p>
-              </div>
-              <div class="item-box">
-                <div class="icon">
-                  <img class="img-icon" alt="web-development" src="https://image.flaticon.com/icons/svg/831/831737.svg" />
-                </div>
-                <h2>Web Development</h2>
-                <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut.</p>
-              </div>
-              
-            </div>
+          <section id="features">
+            <ol>
+              <li className="about-list">
+                RTG NINJA AUTO SHUTDOWN allows you to schedule the system shutdown, with ease. You simply need to enter the desired hour and let the application power off the computer while you are already away. The application is lightweight and can silently run in the background until the time comes for shutting down the PC.
+                Simple to use shutdown scheduler
+              </li>
+              <li>RTG NINJA AUTO SHUTDOWN can come in handy in many situations, all which imply the need to shutdown the system, with a certain delay. If you are away from your desk and cannot shutdown the system, you can simply open RTG NINJA AUTO SHUTDOWN, set the desire hour and leave the application to run on the desktop or in the background.
+                Whether you wish to protect your privacy, save power or control the time your children spend on the computer, this simple application can help you shutdown the computer at the desired time. Moreover, it allows you to set a scheduler, as well as manually give it the command to shutdown, restart or log off.
+                Scheduler or instant command
+              </li>
+              <li>
+                wel
+              </li>
+            </ol>
           </section>
         </div>
       </div>
