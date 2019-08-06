9799e3782b59dd6d61ebd093c1103ae06a18e5ae
Pankaj Goyal <pkjg1807@gmail.com>
2019-08-07 01:10:56 +0530

    final changes

--- src/App.js
+++ src/App.js
@@ -1,60 +1,89 @@
-import React,{Component} from 'react';
-import Header from './Components/Header';
-import Footer from './Components/Footer';
-import Card from './Components/Card';
-import Productlogo from './assets/op.png';
-import SweetAlert from 'sweetalert';
-import './App.css';
-var emails=[]
-class App extends Component{
-  constructor(){
+import React, { Component } from "react";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+import Card from "./Components/Card";
+import Productlogo from "./assets/op.png";
+import SweetAlert from "sweetalert";
+import "./App.css";
+var emails = [];
+class App extends Component {
+  constructor() {
     super();
-    this.state={
-      email:''
-    }
+    this.state = {
+      email: ""
+    };
   }
-  onSearchChange = (e)=>{
-    this.setState({email:e.target.value});
-  }
-  onSubmit = ()=>{
+  onSearchChange = e => {
+    this.setState({ email: e.target.value });
+  };
+  onSubmit = () => {
     emails.push(this.state.email);
-    this.setState({email:''})
-    SweetAlert("Thanks","We got You","Success");
-  }
-  render(){
+    if (this.state.email) 
+      SweetAlert("Thanks", "We got You", "Success");
+    else{
+      SweetAlert("Error","Field is Empty","")
+    }
+    this.setState({ email: "" });
+  };
+  render() {
     return (
       <div className="App">
-
         <Header />
-
         <div className="product">
           <div className="product-heading">
-            <img className="product-image" src={Productlogo} alt="" height="140px" />
+            <img
+              className="product-image"
+              src={Productlogo}
+              alt=""
+              height="140px"
+            />
             <h1> R.T.G. Ninja Shutdown</h1>
           </div>
-          <p style={{"color":"black","fontWeight":"bold"}}>A simple to use application which allows you to easily schedule<br /> the computer shutdown
-            but also trigger the system reboot or
+          <p style={{ color: "black", fontWeight: "bold" }}>
+            A simple to use application which allows you to easily schedule
+            <br /> the computer shutdown but also trigger the system reboot or
             log off on demand.
           </p>
+          <button className="submit">DOWNLOAD</button>
+          <button className="submit">FEEDBACK</button>
         </div>
         <div class="container">
           <section id="features">
-              <Card />
+            <Card />
           </section>
         </div>
 
         <section id="contact">
           <div id="form">
-            <h1 id="form-h1"><p><br />Enter Your Email to get more info.  </p></h1>
-            <input type="email" name="email" id="email" placeholder="Enter your Email" onChange={this.onSearchChange} required />
-            <input type="submit" id="submit" value="Submit" onClick={this.onSubmit}  />
-            
+            <h1 id="form-h1">
+              <p>
+                <br />
+                Enter Your Email to get more info.{" "}
+              </p>
+            </h1>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              value={this.state.email}
+              placeholder="Enter your Email"
+              onChange={this.onSearchChange}
+              required
+            />
+            <input
+              type="submit"
+              className="submit"
+              value="Submit"
+              onClick={this.onSubmit}
+            />
           </div>
         </section>
-        <Footer />
-
+        <h1 style={{"margin":"35px"}}> &lt;<span style={{"fontSize":"60px","margin":"25px"}}>/</span>&gt; Developed By<br /></h1>
+        <h2>Priyanshu Kumar</h2>
+        
+      <Footer />
       </div>
-    ); 
+    );
   }
 }
 
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,23 +1,49 @@
-import React from 'react';
-import '../App.css';
-import logo from '.././op.png';
-import {Navbar,Nav,FormControl,Form,Button} from 'react-bootstrap';
-const Header =()=>{
-	return(
-          	<Navbar className="header" bg="dark" expand="lg">
-			  <a href="/"><Navbar.Brand >News</Navbar.Brand></a>
-			  <Navbar.Toggle aria-controls="basic-navbar-nav" />
-			  <Navbar.Collapse id="basic-navbar-nav">
-			    <Nav className="mr-auto">
-			    </Nav>
-			    <Form inline>
-			    	<div className="menu">
-				    	<span style={{"fontWeight":"bold","padding":"5px"}}><a style={{"fontWeight":"bold","padding":"5px","color":"black","cursor":"pointer"}} href="#features">Features</a></span>
-				    	<span style={{"fontWeight":"bold","padding":"5px"}}>Contact</span>
-				    </div>
-			    </Form>
-			  </Navbar.Collapse>
-			</Navbar>
-		);
-}
+import React from "react";
+import "../App.css";
+import logo from ".././op.png";
+import { Navbar, Nav,Form } from "react-bootstrap";
+const Header = () => {
+  return (
+    <Navbar className="header" bg="" expand="lg">
+      <a href="/">
+        <img className="company-logo" src={logo} height="70px" alt="" />
+        <Navbar.Brand id="company-name">R.T.G Studios</Navbar.Brand>
+      </a>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto" />
+        <Form inline>
+          <div className="menu">
+            <span style={{ fontWeight: "bold", padding: "5px" }}>
+              <a
+                style={{
+                  fontWeight: "bold",
+                  padding: "5px",
+                  color: "black",
+                  cursor: "pointer"
+                }}
+                href="#features"
+              >
+                Features
+              </a>
+            </span>
+            <span style={{ fontWeight: "bold", padding: "5px" }}>
+              <a
+                style={{
+                  fontWeight: "bold",
+                  padding: "5px",
+                  color: "black",
+                  cursor: "pointer"
+                }}
+                href="#contact"
+              >
+                Contact
+              </a>
+            </span>
+          </div>
+        </Form>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+};
 export default Header;
